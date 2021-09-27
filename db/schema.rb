24bc136a38bd688256a820ebf6ff9d14cb831fe6
Elton Santos <eltonaxl@hotmail.com>
2021-09-27 00:50:25 -0300

    Mais coisas para melhorar a aplicaçao

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_26_045244) do
+ActiveRecord::Schema.define(version: 2021_09_26_231558) do
+
+  create_table "cars", force: :cascade do |t|
+    t.string "name"
+    t.string "marca"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cars_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -29,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_09_26_045244) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cars", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Rodando o seed.."
+
+puts "Criando usuários"
+User.create(name: "Admin", email: "admin@admin.com", admin: true, supervisor: false, collaborator: false, password: 123456, role: 0)
+User.create(name: "Supervisor", email: "supervisor@supervisor.com", admin: false, supervisor: true, collaborator: false, password: 123456, role: 1)
+User.create(name: "Colaborador", email: "colaborador@colaborador.com", admin: false, supervisor: false, collaborator: true, password: 123456, role: 2)
+User.create(name: "Superadmin", email: "superadmin@superadmin.com", admin: false, supervisor: false, collaborator: false, password: 123456, role: 0)
+User.create(name: "Auditor", email: "auditor@auditor.com", admin: false, supervisor: false, collaborator: false, password: 123456, role: 1)
+User.create(name: "Gerente", email: "gerente@gerente.com", admin: false, supervisor: false, collaborator: false, password: 123456, role: 2)
+User.create(name: "Elton Santos", email: "elton@elton.com", admin: false, supervisor: false, collaborator: false, password: 123456, role: 3)
+User.create(name: "Ericson Santos", email: "ericson@ericson.com", admin: false, supervisor: false, collaborator: false, password: 123456, role: 3)
+User.create(name: "Eloy Padilha", email: "eloy@eloy.com", admin: false, supervisor: false, collaborator: false, password: 123456, role: 3)
+User.create(name: "Vânia Melo", email: "vania@vania.com", admin: false, supervisor: false, collaborator: false, password: 123456, role: 3)
+
+puts "Criando os carros"
+Car.create(name: "Fit", marca: "Honda", user_id: 7)
+Car.create(name: "Corsa Wind", marca: "Chevrolet", user_id: 7)
+Car.create(name: "Gol", marca: "Volkswagen", user_id: 8)
+Car.create(name: "Corolla", marca: "Toyota", user_id: 8)
+Car.create(name: "Escort", marca: "Ford", user_id: 9)
+Car.create(name: "Golf", marca: "Volkswagen", user_id: 9)
+Car.create(name: "Siena", marca: "Fiat", user_id: 10)
+Car.create(name: "Onix", marca: "Chevrolet", user_id: 10)
