02d6721bfc846e58abb21169b31c8b2cd8f31318
Elton Santos <eltonaxl@hotmail.com>
2022-02-05 02:59:11 -0300

    Removing views desnecessary and preparing to polymorphism

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_053226) do
+ActiveRecord::Schema.define(version: 2022_02_05_055629) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -82,6 +82,17 @@ ActiveRecord::Schema.define(version: 2022_02_05_053226) do
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "comment"
+    t.string "commentable_type", null: false
+    t.integer "commentable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -121,6 +132,7 @@ ActiveRecord::Schema.define(version: 2022_02_05_053226) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "users"
   add_foreign_key "cars", "users"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
   add_foreign_key "profiles", "users"
 end
