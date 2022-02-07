5bb2d4158a1de35351253df8c6f482a9bd800cec
Elton Santos <eltonaxl@hotmail.com>
2022-02-07 01:39:54 -0300

    Trying work JS in cocoon

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
-
+//Added below
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
+//So far
 module.exports = environment
+
