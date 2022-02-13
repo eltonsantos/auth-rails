7d2f0509b3d834b29f0c2ba3c3ae2a32df280f30
Elton Santos <eltonaxl@hotmail.com>
2022-02-13 10:04:18 -0300

    Removing confirmable and lockable in model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   audited
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   enum role: { superadmin: 0, auditor: 1, manager: 2, registred: 3 }
 
