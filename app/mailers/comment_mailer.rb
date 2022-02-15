231051f542e5d771a57731acacfb81b7804864d7
Elton Santos <eltonaxl@hotmail.com>
2022-02-15 03:02:10 -0300

    Send mail to user and change template. All fine

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -3,8 +3,8 @@ class CommentMailer < ApplicationMailer
   default from: 'notifications@example.com'
 
   def comment_moderated(comment_id)
-    comment = Comment.find(comment_id)
-    @user = comment.user
+    @comment = Comment.find(comment_id)
+    @user = @comment.user
     @greeting = "Olá"
     mail from: "support@support.com", to: @user.email, subject: "Notification about your comment"
   end
