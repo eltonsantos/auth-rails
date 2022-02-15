95329e647901560cd2ff660c2bcfd25b8c2c5505
Elton Santos <eltonaxl@hotmail.com>
2022-02-15 00:53:23 -0300

    Moderation comment finished

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,24 @@
 class CommentsController < ApplicationController
 
   def index
-    @comments = Comment.where(approved: false)
+    @comments = Comment.where(approved: false, justify: nil)
   end
 
   def show
     @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to comments_path, notice: "Comment was successfully updated." }
+        format.json { render :index, status: :ok, location: @comment }
+      else
+        format.html { render :show, status: :unprocessable_entity }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def create
@@ -14,6 +27,7 @@ class CommentsController < ApplicationController
     @comment.save
     redirect_to @commentable, notice: 'The comment has been created and will be posted after admin approval.'
   end
+
 
   private
 
